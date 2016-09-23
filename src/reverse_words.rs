3c7a0f7d9d2586958a1b1d8c49485a7d702192e8
ncihnegn <ningchen@outlook.com>
2016-09-23 03:08:05 -0700

    6. Reverse words in-place

--- src/reverse_words.rs
+++ src/reverse_words.rs
@@ -1,21 +1,53 @@
 pub fn reverse_words(s: &str) -> String {
-    let sstr = s.to_string();
-    let rws = sstr.split_whitespace();
+    let rws = s.split_whitespace();
     let mut r = "".to_string();
-    for sub in rws {
-        r = r + &sub.chars().rev().collect::<String>() + " ";
+    for sub in rws.rev() {
+        r = r + sub + " ";
     }
-    let len = r.len()-1;
-    r.truncate(len);
-    r
+    r.trim().to_string()
+}
+
+pub fn reverse_words_inplace(s: &str) -> String {
+    let mut chars: Vec<char> = s.chars().collect();
+    let mut index_forward = 0;
+    let mut index_backward = if chars.is_empty() { 0 } else { chars.len()-1 };
+    while index_forward < index_backward {
+        chars.swap(index_forward, index_backward);
+        index_forward = index_forward+1;
+        index_backward = index_backward-1;
+    }
+    index_forward = 0;
+    for i in 0..chars.len()+1 {
+        if i == chars.len() || chars[i] == ' ' {
+            let next_begin = i + 1;
+            index_backward = if i > 0 { i-1 } else { 0 };
+            while index_forward < index_backward {
+                chars.swap(index_forward, index_backward);
+                index_forward = index_forward+1;
+                index_backward = index_backward-1;
+            }
+            index_forward = next_begin;
+        }
+    }
+    chars.into_iter().collect::<String>()
 }
 
 #[cfg(test)]
 mod test {
     use super::reverse_words;
+    use super::reverse_words_inplace;
     #[test]
     fn example() {
-        assert_eq!(reverse_words("test"), "tset".to_string());
-        assert_eq!(reverse_words("test t"), "tset t".to_string());
+        assert_eq!(&reverse_words("test"), "test");
+        assert_eq!(&reverse_words("test t"), "t test");
+        assert_eq!(&reverse_words(""), "");
+        assert_eq!(&reverse_words(" test "), "test");
+        assert_eq!(&reverse_words(" test  t "), "t test");
+
+        assert_eq!(&reverse_words_inplace("test"), "test");
+        assert_eq!(&reverse_words_inplace("test t"), "t test");
+        assert_eq!(&reverse_words_inplace(""), "");
+        assert_eq!(&reverse_words_inplace(" test "), " test ");
+        assert_eq!(&reverse_words_inplace(" test  t "), " t  test ");
     }
 }
