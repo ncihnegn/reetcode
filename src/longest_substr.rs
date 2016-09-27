4a85a0f4d13aca30db7a7eeef1adb1e79ed25a2d
ncihnegn <ningchen@outlook.com>
2016-09-27 15:30:32 -0700

    11. Longest substring with at most two dist

--- src/longest_substr.rs
+++ src/longest_substr.rs
@@ -1,20 +1,35 @@
-pub fn longest_substr(s: &str) -> String {
+pub fn longest_substr(s: &str) -> String {//No repeating character
     let v = s.chars().collect::<Vec<char>>();
-    let mut cand = Vec::new();
     let mut work = Vec::new();
+    let mut cand = work.clone();
     for c in v {
         let p = work.iter().position(|&x| x == c);
-        match p {
-            None => work.push(c),
-            Some(index) => {
-                if work.len() > cand.len() {
-                    cand = work.clone();
-                }
-                work.push(c);
-                for i in 0..index+1 {
-                    work.remove(i);
-                }
+        if let Some(index) = p {
+            work = work.split_off(index+1);
+        }
+        work.push(c);
+        if work.len() > cand.len() {
+            cand = work.clone();
+        }
+    }
+    cand.into_iter().collect::<String>()
+}
+
+pub fn longest_substr_two(s: &str) -> String {//At most two distinct characters
+    let limit = 2;
+    let v = s.chars().collect::<Vec<char>>();
+    let mut work = Vec::new();
+    let mut cand = work.clone();
+    for c in v {
+        if work.len() >= limit && !work.iter().all(|&x| x == work[0]) {
+            if !work.iter().any(|&x| x == c) {
+                let index = work.iter().rposition(|&x| x == work[0]).unwrap();
+                work = work.split_off(index+1);
             }
+        }
+        work.push(c);
+        if work.len() > cand.len() {
+            cand = work.clone();
         }
     }
     cand.into_iter().collect::<String>()
@@ -23,11 +38,23 @@ pub fn longest_substr(s: &str) -> String {
 #[cfg(test)]
 mod test {
     use super::longest_substr;
+    use super::longest_substr_two;
 
     #[test]
     fn example() {
+        assert_eq!(&longest_substr(""), "");
+        assert_eq!(&longest_substr("a"), "a");
         assert_eq!(&longest_substr("abcabcbb"), "abc");
+        assert_eq!(&longest_substr("abcabcdbb"), "abcd");
         assert_eq!(&longest_substr("bbbbb"), "b");
         assert_eq!(&longest_substr("abcabcdbb"), "abcd");
+
+        assert_eq!(&longest_substr_two("eceba"), "ece");
+        assert_eq!(&longest_substr_two("ec"), "ec");
+        assert_eq!(&longest_substr_two("eec"), "eec");
+        assert_eq!(&longest_substr_two("e"), "e");
+        assert_eq!(&longest_substr_two(""), "");
+        assert_eq!(&longest_substr_two("ecebabb"), "babb");
+        assert_eq!(&longest_substr_two("ecebabbec"), "babb");
     }
 }
